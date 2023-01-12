c65579b6e653da57a6bf628e1dbe09f5dcee5db8
marshtv <marshtv.1972@gmail.com>
2023-01-12 04:31:04 +0300

    Complete_Alpha

--- main.cpp
+++ main.cpp
@@ -1,6 +1,212 @@
 #include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <thread>
+#include <mutex>
+
+class Swimmer {
+private:
+	std::string name = "unknown";
+	double speed = 1.0;
+public:
+	Swimmer(std::string in_name, double in_speed) {
+		this->name = in_name;
+		assert(in_speed >= 0);
+		this->speed = in_speed;
+	}
+
+	std::string getName() {
+		return this->name;
+	}
+
+	double getSpeed() {
+		return this->speed;
+	}
+};
+
+class SwimTrack {
+private:
+	int count = 0;
+	double length = 100;
+	Swimmer* swimmer = nullptr;
+public:
+	SwimTrack(int in_count, double in_length, std::string in_name, double in_speed) {
+		assert(in_count >= 0);
+		this->count = in_count;
+		assert(in_length >= 0);
+		this->length = in_length;
+		this->swimmer = new Swimmer(in_name, in_speed);
+	}
+
+	SwimTrack(const SwimTrack &other) {
+		this->count = other.count;
+		this->length = other.length;
+		this->swimmer = new Swimmer(other.swimmer->getName(), other.swimmer->getSpeed());
+	}
+
+	~SwimTrack() {
+		delete this->swimmer;
+	}
+
+	int getCount() {
+		return this->count;
+	}
+
+	double getLength() {
+		return this->length;
+	}
+
+	Swimmer* getSwimmer() {
+		return this->swimmer;
+	}
+
+	int getFinishSeconds() {
+		return (int)std::round(this->length / this->getSwimmer()->getSpeed());
+	}
+};
+
+class Pool {
+private:
+	int tracksNum = 6;
+	double length = 100;
+	SwimTrack** tracks = nullptr;
+public:
+	Pool(int in_tracksNum, double in_length) {
+		assert(in_tracksNum >= 0);
+		this->tracksNum = in_tracksNum;
+		assert(in_length >= 0);
+		this->length = in_length;
+		if (in_tracksNum > 0 && in_length > 0) {
+			tracks = new SwimTrack*[in_tracksNum];
+			for (int i = 0; i < in_tracksNum; ++i) {
+				std::cout << "Track #" << i << std::endl;
+				std::cout << "Input swimmer's name and his speed (metre/sec):";
+				std::string in_name;
+				double in_speed;
+				std::cin >> in_name >> in_speed;
+				SwimTrack* swimTrack = new SwimTrack(i, this->length, in_name, in_speed);
+				this->tracks[i] = swimTrack;
+			}
+		}
+	}
+
+	Pool(const Pool &other) {
+		this->tracksNum = other.tracksNum;
+		this->length = other.length;
+		assert(other.tracks != nullptr);
+		this->tracks = new SwimTrack*[this->tracksNum];
+		for (int i = 0; i < this->tracksNum; ++i) {
+			this->tracks[i] = other.tracks[i];
+		}
+	}
+
+	~Pool() {
+		delete[] this->tracks;
+	}
+
+	int getTracksNum() {
+		return this->tracksNum;
+	}
+
+	double getLength() {
+		return this->length;
+	}
+
+	SwimTrack* getTrackByCount(int in_count) {
+		if(this->tracks == nullptr) return nullptr;
+		else return this->tracks[in_count];
+	}
+};
+
+struct tablo {
+	int trackNum = 0;
+	std::string name = "unknown";
+	double swimmedLength = 0;
+	bool bIsFinished = false;
+};
+
+std::vector<tablo> swimScoreboard;
+std::mutex swimScoreboard_mutex;
+std::vector<tablo> tracksWinList;
+std::mutex tracksWinList_mutex;
+
+void startSwim(SwimTrack* track) {
+	swimScoreboard_mutex.lock();
+	swimScoreboard[track->getCount()].trackNum = track->getCount();
+	swimScoreboard[track->getCount()].name = track->getSwimmer()->getName();
+	swimScoreboard[track->getCount()].bIsFinished = false;
+	swimScoreboard[track->getCount()].swimmedLength = 0;
+	while (!swimScoreboard[track->getCount()].bIsFinished) {
+		swimScoreboard_mutex.unlock();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		swimScoreboard_mutex.lock();
+		swimScoreboard[track->getCount()].swimmedLength += track->getSwimmer()->getSpeed();
+		if (swimScoreboard[track->getCount()].swimmedLength >= track->getLength()) {
+			swimScoreboard[track->getCount()].bIsFinished = true;
+			std::cout << swimScoreboard[track->getCount()].name << " FINISHED." << std::endl;
+			tracksWinList_mutex.lock();
+			tracksWinList.push_back(swimScoreboard[track->getCount()]);
+			tracksWinList_mutex.unlock();
+		} else
+			swimScoreboard[track->getCount()].bIsFinished = false;
+	}
+	swimScoreboard_mutex.unlock();
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Input swimming tracks number and track's length:";
+	int in_tracksNum;
+	double in_length;
+	std::cin >> in_tracksNum >> in_length;
+	auto* pool = new Pool(in_tracksNum, in_length);
+
+	std::cout << "-------------------------------------------------" << std::endl;
+	std::cout << "list:" << std::endl;
+	std::cout << "-------------------------------------------------" << std::endl;
+
+	for (int i = 0; i < pool->getTracksNum(); ++i) {
+		std::cout << "Track #" << pool->getTrackByCount(i)->getCount() << " "
+					<< pool->getTrackByCount(i)->getSwimmer()->getName() << " "
+					<< "speed: " << pool->getTrackByCount(i)->getSwimmer()->getSpeed() << std::endl;
+	}
+	std::cout << "-------------------------------------------------" << std::endl;
+
+	for (int i = 0; i < pool->getTracksNum(); ++i) {
+		tablo in_tablo;
+		swimScoreboard.push_back(in_tablo);
+	}
+
+	std::cout << "\n\t\tAttention...." << std::endl;
+	for (int i = 3; i >= 0; --i) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		if (i > 0)
+			std::cout << "\t\t" << i << " ...." << std::endl;
+		else {
+			std::cout << "\t\tSTART" << std::endl;
+			break;
+		}
+	}
+	std::cout << "------------------------------------" << std::endl;
+
+	for (int i = 0; i < pool->getTracksNum(); ++i) {
+		std::thread start(startSwim, pool->getTrackByCount(i));
+		start.detach();
+		//if (start.joinable()) start.join();
+		start.join();
+	}
+
+	std::cout << "Winner's Scoreboard:" << std::endl;
+	std::cout << "------------------------------------" << std::endl;
+	for (int i = 0; i < tracksWinList.size(); ++i) {
+		std::cout 	<< i + 1 << ". "
+					<< pool->getTrackByCount(tracksWinList[i].trackNum)->getSwimmer()->getName()
+					<< ", Time: "
+					<< pool->getTrackByCount(tracksWinList[i].trackNum)->getFinishSeconds() << std::endl;
+	}
+	std::cout << "------------------------------------" << std::endl;
+	std::cout << "Exit program. Good by." << std::endl;
+
 	return 0;
 }
